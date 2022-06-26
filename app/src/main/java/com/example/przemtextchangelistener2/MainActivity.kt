eac2763c1799a11560887727fd204a57885736d9
Przemyslaw Korecki <Przemyslaw.Korecki@mag.pl>
2022-06-26 14:23:06 +0200

    first commit

---
+++ app/src/main/java/com/example/przemtextchangelistener2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.przemtextchangelistener2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        editText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textView.text = (p0.toString())
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+}
